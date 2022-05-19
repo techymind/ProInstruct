44633b2c64c43c6bb5f726fc0018aa614f69dbe1
anudeep <anudeep@amadeusitc32839.onmicrosoft.com>
2022-05-19 20:44:33 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
+import { TutorSignupComponent } from './tutor-signup/tutor-signup.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+
+const routes=[
+  {path:'home',component:HomeComponent},
+  {path:'tutorsignup',component:TutorSignupComponent}
+];
 
 @NgModule({
   declarations: [
@@ -10,8 +18,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/tutor-signup/tutor-signup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutor-signup',
+  templateUrl: './tutor-signup.component.html',
+  styleUrls: ['./tutor-signup.component.css']
+})
+export class TutorSignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
